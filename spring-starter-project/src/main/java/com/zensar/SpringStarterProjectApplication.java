af0c2fac2d6adab19f966ef6c4bbd3df182f00fc
SR43993 <SR43993@TRGW42SLPC3305.mshome.net>
2020-02-14 12:32:58 +0530

    Added Actuator,Security,Changed name of application.properties etc

--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -1,13 +1,18 @@
 package com.zensar;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringStarterProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterProjectApplication.class, args);
+		//SpringApplication.run(SpringStarterProjectApplication.class, args);
+		
+		new SpringApplicationBuilder(SpringStarterProjectApplication.class)
+		.properties("spring.config.name:application")
+		.properties("server.port:5000")
+		.run(args);
 	}
 
 }
